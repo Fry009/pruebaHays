6b7a54bea27f183c179df768084b5218c096fd52
fmdiazmontes87 <fmdiazmontes87@gmail.com>
2025-07-15 13:07:49 +0200

    report edit dialog

---
+++ pruebaHaysFrontApp/src/app/features/reports/components/report-edit-dialog/report-edit-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-edit-dialog',
+  imports: [],
+  templateUrl: './report-edit-dialog.component.html',
+  styleUrl: './report-edit-dialog.component.scss'
+})
+export class ReportEditDialogComponent {
+
+}
